ea28f0633a1f857340bda674ca4e5a3fe0ed029e
zarembapawel <zarembapawel95@gmail.com>
2023-08-19 12:54:49 +0200

    Add Spring Boot

---
+++ src/test/java/pl/zarembapawel/customers/CustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zarembapawel.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
